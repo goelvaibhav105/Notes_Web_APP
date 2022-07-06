7cad90729f904043b56f1ebd4a36258cd9a706ac
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-06 15:49:18 +0530

    change storage except if text is same

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -5,6 +5,9 @@ export default function Note(props) {
 
   const noteTextUpdated = (event) => {
     const newTextValue = event.currentTarget.textContent;
+    if(newTextValue === props.note.text){
+      return ;
+    }
     const updatedNoteObject = {...props.note,text:newTextValue || ''}
     props.onNoteUpdate(updatedNoteObject)
   }
