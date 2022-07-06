b82a405e5ed0f1618a1d34d1eeac8de10d1292d3
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-06 12:33:20 +0530

    putting some dummy notes and fetched them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,34 @@
 import './App.css';
 import axios from 'axios'
 import { useState,useEffect } from 'react';
+import DUMMY_NOTES from './DUMMY_NOTES';
 
 
 function App() {
   const  [notesList,setNotesList] = useState([]);
-  const getNotes = async () => {
-    try {
-      const response = await axios.get(
-        'http://localhost:5000/notes'
-      )
-      setNotesList(response.data.notes)
-      console.log('notesList',notesList,notesList.length);
-    } catch (err) {
-      console.log(err)
-    }
-  }
+
+  //so what happen when we set notes ... then it will go into a loopp which is then bypass by using a useEffect method
+  
+  useEffect (()=>{
+    setNotesList(DUMMY_NOTES)
+  },[])
+  
+  
+  // const getNotes = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       'http://localhost:5000/notes'
+  //     )
+  //     setNotesList(response.data.notes)
+  //     console.log('notesList',notesList,notesList.length);
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
   return (
     <div className="App">
       <h1>Notes Application</h1>
-      <div>
-        <button onClick={getNotes}>
-          Click me !
-        </button>
-      </div>
      {notesList.length > 0 ?  <div>
         <h4>SHOW DETAILS</h4>
          <h5>{notesList ? notesList[0].text : ''}</h5>
---
+++ frontend/src/DUMMY_NOTES.js
@@ -0,0 +1,15 @@
+
+const DUMMY_NOTES = [
+  { _id: "1", text: "vaibhav's Twitch", link: "https://twitch.tv/codewithvaibhav" },
+  { _id: "2", text: "Get a job", link: "https://rozee.pk" },
+  { _id: "3", text: "vaibhav's Twitch", link: "https://twitch.tv/codewithvaibhav" },
+  { _id: "4", text: "Get a job", link: "https://rozee.pk" },
+  { _id: "5", text: "vaibhav's Twitch", link: "https://twitch.tv/codewithvaibhav" },
+  { _id: "6", text: "Get a job", link: "https://rozee.pk" },
+  { _id: "7", text: "vaibhav's Twitch", link: "https://twitch.tv/codewithvaibhav" },
+  { _id: "8", text: "Get a job", link: "https://rozee.pk" },
+  { _id: "9", text: "vaibhav's Twitch", link: "https://twitch.tv/codewithvaibhav" },
+  { _id: "10", text: "Get a job", link: "https://rozee.pk" },
+];
+
+export default DUMMY_NOTES;
