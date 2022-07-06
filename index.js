c56ecd7001c26ffc263c9007312d313c94a54483
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-06 09:33:29 +0530

    listnening to /

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = 5000;
 
 
 app.listen(port,()=>{
-
     console.log("NOtes backend is running on port")
 })
+
+// root (/) THIS IS THE FIRST STEP WE HAVE TO DO 
+app.get("/", (request, response) => {
+    response.send("Hello world!");
+  });
+
+
