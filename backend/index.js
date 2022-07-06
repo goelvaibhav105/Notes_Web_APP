d7ba5d7e524bea2415b12d359ebb7759d71c8748
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-06 10:30:25 +0530

    first api call from frontend to backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express();
 const port = 5000;
 
+// for allowing the other domin to access we need to prorvide access in porod we have not to 
+ // like this but whitelist the urls
+const cors = require('cors')
+
+app.use(cors());
+
 
 app.listen(port,()=>{
     console.log("NOtes backend is running on port")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const getNotes = async () => {
+    try {
+      const response = await axios.get(
+        'http://localhost:5000/notes'
+      )
+      console.log(response,"response")
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notes Application</h1>
+      <div>
+        <button onClick={getNotes}>
+          Click me !
+        </button>
+      </div>
     </div>
   );
 }
