7d1c4499cf37bcb9cf79b5f85c7f658921f7b027
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-06 14:25:13 +0530

    frontend component

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -2,10 +2,21 @@ import React from 'react'
 import '../style/Note.css';
 
 export default function Note(props) {
+
+  const noteTextUpdated = (event) => {
+    console.log(event.currentTarget.textContent, "textCHNAGED ")
+  }
+
   return (
-    <div className='note'>
-        <div className='note__text'>{props.note.text}</div>
-        <div className='note__link'>{props.note.link}</div>
+    <div className='note' >
+      <div className='note__text'
+        onBlur={noteTextUpdated}
+        contentEditable={true}
+        suppressContentEditableWarning={true}
+      >
+        {props.note.text}
+      </div>
+      <div className='note__link'>{props.note.link}</div>
     </div>
   )
 }
