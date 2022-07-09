657e8efb86e49c530f0e4e3aeb01f1f21059c210
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-09 12:00:56 +0530

    connecting to the cloud DB

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,9 +1,33 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/notes-db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost:27017/notes-db", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// connect to mongo atlas prod database
+
+const { DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } =
+  process.env;
+
+
+//eg..
+
+// mongoose.connect("mongodb+srv://userName:passowrd@cluster0.y2jmu.mongodb.net/notes-db?retryWrites=true&w=majority", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+
+// So all Things are coming from env file 
+
+mongoose.connect(
+  `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/${DATABASE_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 
 // checking connection with
