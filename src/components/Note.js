40bd8442af348780ef128e6e31b09e77eb9ec448
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-10 11:19:11 +0530

    Style fix

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../style/Note.css';
 
 export default function Note(props) {
-
+  const [isFocused, setIsFocused] = useState(false)
   const noteTextUpdated = (event) => {
+    setIsFocused(false);
     const newTextValue = event.currentTarget.textContent;
     if(newTextValue === props.note.text){
       return ;
@@ -11,12 +12,16 @@ export default function Note(props) {
     const updatedNoteObject = {...props.note,text:newTextValue || ''}
     props.onNoteUpdate(updatedNoteObject)
   }
+  console.log(isFocused,"isFocused")
 
 
   return (
-    <div className='note' >
+    <div className={isFocused ? "note note--focused" : "note"}>
       <div className='note__text'
         onBlur={noteTextUpdated}
+        onFocus={()=>{
+setIsFocused(true)
+        }}
         contentEditable={true}
         suppressContentEditableWarning={true}
       >
