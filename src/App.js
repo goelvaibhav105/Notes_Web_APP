1be55ff9d164df2edc835caeeb73caff11fcd826
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-10 15:06:54 +0530

    Add Note Funcionality Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react';
 // import DUMMY_NOTES from './DUMMY_NOTES';
 import Note from './components/Note';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   createNote,
   deleteNote,
@@ -10,9 +11,31 @@ import {
   updateNote,
 } from "./services/notesService";
 
+import { Button, FloatingLabel, Modal, Form } from "react-bootstrap";
+
 
 function App() {
   const [notesList, setNotesList] = useState([]);
+  const [showAddNoteModal, setShowAddNoteModal] = useState(false);
+  const [newNote, setNewNote] = useState({
+    link: "",
+    text: "",
+  });
+
+
+  const handleCloseAddModal = () => {
+    // we are setting them as empty things bacuse suppose someone create a NOTE 1 ,
+     // and save it then if we dont set it null then Note 1 will be in a Prev state and 
+      // will impose and create a same note ... So we have to set it  nulll when we close it 
+    setNewNote({
+      link: "",
+      text: "",
+    });
+    setShowAddNoteModal(false);
+  };
+  const handleShowAddModal = () => setShowAddNoteModal(true);
+
+  
 
   //so what happen when we set notes ... then it will go into a loopp which is then bypass by using a useEffect method
 
@@ -35,6 +58,13 @@ function App() {
     }
   }, [notesList]);
 
+  const addNote = async () => {
+    const savedNote = await createNote(newNote);
+    // this is setting on frountend means previous noteList and current note both are merged and saved 
+    setNotesList([...notesList, savedNote]);
+    handleCloseAddModal();
+  };
+
 
   const updateNoteItem = (updatedNote) =>{
    //map fnc will create the new array means updated array
@@ -49,6 +79,63 @@ function App() {
 
   return (
     <div className="App">
+            <Button
+        variant="dark"
+        className="add-button"
+        onClick={handleShowAddModal}
+      >
+        <div className="add-button-text">+</div>
+      </Button>
+
+      <Modal show={showAddNoteModal} onHide={handleCloseAddModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Note</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FloatingLabel controlId="floatingTextarea2" label="Text">
+            <Form.Control
+            // here we are setting our new note 
+              onChange={(event) => {
+                const newVal = event.currentTarget.value;
+                setNewNote({
+                  // we are taking other value same and updating text property 
+                  ...newNote,
+                  text: newVal,
+                });
+              }}
+              as="textarea"
+              placeholder="Enter your note text"
+              style={{ height: "100px" }}
+            />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingTextarea"
+            label="Link"
+            className="mb-3 note-link"
+          >
+            <Form.Control
+              onChange={(event) => {
+                const newVal = event.currentTarget.value;
+                setNewNote({
+                         // keep all other value same and updating link value 
+                  ...newNote,
+                  link: newVal,
+                });
+              }}
+              type="url"
+              placeholder="Enter note url"
+            />
+          </FloatingLabel>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAddModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={addNote}>
+            Create
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className='headerTop'>
         <h3>Save Your Notes</h3>
         <span>You can save anything you want</span>
