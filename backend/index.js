981a3e366c3757007dcad98d90f67e486a2e6436
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-10 10:21:37 +0530

    Deploy CHnages ... deployed using heroku

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; // definte a port
 const cors = require('cors')
 
 var router = express.Router()
